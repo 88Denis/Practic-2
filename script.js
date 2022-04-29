51da72fcda854d21ca0dbf368346cd3eb8c433a5
Denis <denis-serov00@mail.ru>
2022-04-29 14:48:13 +0600

    Работа с localStorage

--- script.js
+++ script.js
@@ -437,7 +437,61 @@
 // const res = arr.reduce((sum, current) => sum + current);
 // console.log(res);
 
-const arr = ['apple', 'pear', 'plum'];
+// const arr = ['apple', 'pear', 'plum'];
 
-const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-console.log(res);
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+
+  ////////////////////////////////////////////
+ ////////* Работа с localStorage *///////////
+////////////////////////////////////////////
+
+// setItem - создает объект (ключт/значение)
+// getItem - получает данные из localStorage
+// removeItem - удаляет элемент из localStorage
+// clear - чистит все локальное хранилище
+
+
+// localStorage.setItem('number', 5);
+
+//  localStorage.removeItem('number');
+// localStorage.clear();
+
+// console.log(localStorage.getItem('number'));
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+  checkbox.checked = true;
+}
+if (localStorage.getItem('bg') === 'changed'){
+  
+  form.style.backgroundColor = '#red';
+}
+
+checkbox.addEventListener('change', () => {
+  localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+  if (localStorage.getItem('bg') === 'changed'){
+    localStorage.removeItem('bg');
+    form.style.backgroundColor = '#fff';
+  } else {
+    localStorage.setItem('bg', 'changed');
+    form.style.backgroundColor = 'red';
+  }
+});
+
+const persone = {
+  name: 'Alex',
+  age: 25
+};
+
+const serializefPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializefPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
